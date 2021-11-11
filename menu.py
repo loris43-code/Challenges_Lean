afc3efdd82e6448845edfbe9c46b1b592b5bfc3a
Loris Nagles <loris_43@hotmail.com>
2021-11-11 10:28:44 -0500

    Project is added

---
+++ menu.py
@@ -0,0 +1,32 @@
+#menu = ("\n***Challenge C# Junior Developer***\n Select an option\n2. Find Multiples of 3 or 5 \n3. Find Power Number \n4. Make Matriz and sum diagonals \n5. Make Program to access database")
+
+menu = print("***Challenge C# Junior Developer***")
+print("")
+print("Select an option:")
+print("")
+print("1. Find Multiples of 3 or 5")
+print ("2. Find Power Number")
+print ("3. Make Matriz and sum diagonal")
+print ("4. Make Program to acces database")
+print ("0. Exit the program")
+print("")
+option = int(input ("Option: "))
+
+while option !=0:        
+    if (option == 1) :
+        print("You selected #1")
+            
+    elif (option == 2) :
+        print ("You selected #2")
+                
+    elif (option == 3) :
+        print("You selected #3")
+    else:
+        print("Invalid option.")
+
+    option = int(input ("Option: "))
+                
+            
+            
+    
+            
